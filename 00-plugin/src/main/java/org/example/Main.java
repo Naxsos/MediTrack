8f74b559281082faf53d422f85427af5be667a24
Naxsos <112490751+Naxsos@users.noreply.github.com>
2025-03-01 17:32:10 +0100

    Refactoring: Middle Man

--- 00-plugin/src/main/java/org/example/Main.java
+++ 00-plugin/src/main/java/org/example/Main.java
@@ -11,22 +11,21 @@ import java.util.TimeZone;
 public class Main {
     public static void main(String[] args) throws SchedulerException {
         DateiMedikamenteSpeicher dateiSpeicher = new DateiMedikamenteSpeicher("test.txt") ;
-        DateiMedikamenteSpeicherAdapter dateiSpeicherAdapter = new DateiMedikamenteSpeicherAdapter(dateiSpeicher);
 
-        ErstelleMedikamentUseCase erstellenService = new ErstelleMedikamentUseCase(dateiSpeicherAdapter);
-        EntferneMedikamentUseCase entfernenService = new EntferneMedikamentUseCase(dateiSpeicherAdapter);
-        AlleMedikamenteAusgebenUseCase listeAllerMedikamenteUseCase = new AlleMedikamenteAusgebenUseCase(dateiSpeicherAdapter);
-        ListeInZeitraumAblaufenderMedikamenteUseCase baldAblaufendeMedikamenteUseCase = new ListeInZeitraumAblaufenderMedikamenteUseCase(dateiSpeicherAdapter);
-        AbgelaufeneMedikamenteUseCase abgelaufeneMedikamentUseCase = new AbgelaufeneMedikamenteUseCase(dateiSpeicherAdapter);
+        ErstelleMedikamentUseCase erstellenService = new ErstelleMedikamentUseCase(dateiSpeicher);
+        EntferneMedikamentUseCase entfernenService = new EntferneMedikamentUseCase(dateiSpeicher);
+        AlleMedikamenteAusgebenUseCase listeAllerMedikamenteUseCase = new AlleMedikamenteAusgebenUseCase(dateiSpeicher);
+        ListeInZeitraumAblaufenderMedikamenteUseCase baldAblaufendeMedikamenteUseCase = new ListeInZeitraumAblaufenderMedikamenteUseCase(dateiSpeicher);
+        AbgelaufeneMedikamenteUseCase abgelaufeneMedikamentUseCase = new AbgelaufeneMedikamenteUseCase(dateiSpeicher);
 
-        erstellenService.erstelleMedikament(21234567,"ABC1234567890XYZ", "BATCH001", "Kreativer Name mit ganz vielen xyz", "Zanax", "2025-01");
+        erstellenService.erstelleMedikament(21234567,"ABC1234567890XYZ", "BATCH001", "Kreativer Name mit ganz vielen xyz", "Zanax", "2025-04");
         erstellenService.erstelleMedikament(23414564, "XYZ1234234290ABC", "BATCH004", "Kreativer Name mit ganz vielen abx", "Soledum", "2024-06");
         List<Medikament> liste = listeAllerMedikamenteUseCase.listeAllerMedikamente();
-        SendeErinnerungMedikamentlaeuftAb sendeErinnerungMedikamentlaeuftAb = new SendeErinnerungMedikamentlaeuftAb(dateiSpeicherAdapter);
+        SendeErinnerungMedikamentlaeuftAb sendeErinnerungMedikamentlaeuftAb = new SendeErinnerungMedikamentlaeuftAb(dateiSpeicher);
         sendeErinnerungMedikamentlaeuftAb.sendeErinnerungen();
         entfernenService.medikamentEntfernen("01 23414564 21 XYZ1234234290ABC 10 BATCH004 17 06-2025");
 
-        /*SwingUtilities.invokeLater(() ->
+        SwingUtilities.invokeLater(() ->
                 new MediTrackUI(
                     erstellenService,
                     listeAllerMedikamenteUseCase,
@@ -34,9 +33,9 @@ public class Main {
                     baldAblaufendeMedikamenteUseCase,
                     abgelaufeneMedikamentUseCase
                 )
-        );*/
-        NotionBenachrichtigung notionBenachrichtigung = new NotionBenachrichtigung();
-        notionBenachrichtigung.sendeNotionBenachrichtigung("test","2525-12");
+        );
+        //NotionBenachrichtigung notionBenachrichtigung = new NotionBenachrichtigung();
+        //notionBenachrichtigung.sendeNotionBenachrichtigung("test","2525-12");
         //AllgemeinerScheduler allgemeinerScheduler = new AllgemeinerScheduler(dateiSpeicherAdapter);
 
 
