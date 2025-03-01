b53014361a10d499e7c17884bcda877eca085925
Naxsos <112490751+Naxsos@users.noreply.github.com>
2025-03-01 17:31:14 +0100

    Refactoring: Long Parameter List, Primitive Obsession

--- 03-domain/src/main/java/org/example/Medikament.java
+++ 03-domain/src/main/java/org/example/Medikament.java
@@ -6,136 +6,60 @@ import java.util.Objects;
 
 public class Medikament {
 
-    // Eindeutige Identifikation
-    private UniqueIdentifier ui;
+    private final UniqueIdentifier ui;
+    private final Seriennummer serienNummer;
+    private final PZN pzn;
+    private final Chargennummer chargenNummer;
+    private final String medikamentenName;
+    private final String wirkstoffBezeichnung;
+    private final String hersteller;
+    private final Darreichungsform darreichungsform;
+    private final Dosierung dosierung;
+    private final YearMonth ablaufDatum;
 
-    private String serienNummer;
-
-    private int pzn;
-
-    private String chargenNummer;
-
-    private String medikamentenName;
-
-    private String wirkstoffBezeichnung;
-
-    private String hersteller;
-
-    private Darreichungsform darreichungsform;
-
-    private Dosierung dosierung;
-
-    private YearMonth ablaufDatum;
-
-    public Medikament(UniqueIdentifier ui, int pzn, String serienNummer, String chargenNummer, String medikamentenName, String wirkstoffBezeichnung, String hersteller, Darreichungsform darreichungsform, Dosierung dosierung, YearMonth ablaufDatum) {
-        if (ui == null) {
-            throw new IllegalArgumentException("Pharmazeutische Kennung darf nicht leer sein");
-        }
-        this.ui = ui;
-        this.serienNummer = serienNummer;
-        this.chargenNummer = chargenNummer;
-        this.pzn = pzn;
-        this.medikamentenName = medikamentenName;
-        this.wirkstoffBezeichnung = wirkstoffBezeichnung;
-        this.hersteller = hersteller;
-        this.darreichungsform = darreichungsform;
-        this.dosierung = dosierung;
-        this.ablaufDatum = ablaufDatum;
-    }
-
-    public Medikament(UniqueIdentifier ui, int pzn, String serienNummer, String chargenNummer, String medikamentenName, String wirkstoffBezeichnung, YearMonth ablaufDatum) {
-        if (ui == null) {
-            throw new IllegalArgumentException("Pharmazeutische Kennung darf nicht leer sein");
-        }
-        this.ui = ui;
-        this.serienNummer = serienNummer;
-        this.chargenNummer = chargenNummer;
-        this.pzn = pzn;
-        this.medikamentenName = medikamentenName;
-        this.wirkstoffBezeichnung = wirkstoffBezeichnung;
-        this.ablaufDatum = ablaufDatum;
+    private Medikament(Builder builder) {
+        this.ui = builder.ui;
+        this.serienNummer = builder.serienNummer;
+        this.pzn = builder.pzn;
+        this.chargenNummer = builder.chargenNummer;
+        this.medikamentenName = builder.medikamentenName;
+        this.wirkstoffBezeichnung = builder.wirkstoffBezeichnung;
+        this.hersteller = builder.hersteller;
+        this.darreichungsform = builder.darreichungsform;
+        this.dosierung = builder.dosierung;
+        this.ablaufDatum = builder.ablaufDatum;
     }
 
     public UniqueIdentifier getUi() {
         return ui;
     }
-
-    public String getSerienNummer() {
+    public Seriennummer getSerienNummer() {
         return serienNummer;
     }
-
-    public void setSerienNummer(String serienNummer) {
-        this.serienNummer = serienNummer;
-    }
-
-    public int getPzn() {
+    public PZN getPzn() {
         return pzn;
     }
-
-    public void setPzn(int pzn) {
-        this.pzn = pzn;
-    }
-
-    public String getChargenNummer() {
+    public Chargennummer getChargenNummer() {
         return chargenNummer;
     }
-
-    public void setChargenNummer(String chargenNummer) {
-        this.chargenNummer = chargenNummer;
-    }
-
     public String getMedikamentenName() {
         return medikamentenName;
     }
-
-    public void setMedikamentenName(String medikamentenName) {
-        this.medikamentenName = medikamentenName;
-    }
-
     public String getWirkstoffBezeichnung() {
         return wirkstoffBezeichnung;
     }
-
-    public void setWirkstoffBezeichnung(String wirkstoffBezeichnung) {
-        this.wirkstoffBezeichnung = wirkstoffBezeichnung;
-    }
-
     public String getHersteller() {
         return hersteller;
     }
-
-    public void setHersteller(String hersteller) {
-        this.hersteller = hersteller;
-    }
-
     public Darreichungsform getDarreichungsform() {
         return darreichungsform;
     }
-
-    public void setDarreichungsform(Darreichungsform darreichungsform) {
-        this.darreichungsform = darreichungsform;
-    }
-
     public Dosierung getDosierung() {
         return dosierung;
     }
+    public YearMonth getAblaufDatum() {return ablaufDatum;}
 
-    public void setDosierung(Dosierung dosierung) {
-        this.dosierung = dosierung;
-    }
-
-    public YearMonth getAblaufDatum() {
-        return ablaufDatum;
-    }
-
-    public void setAblaufDatum(YearMonth ablaufDatum) {
-        this.ablaufDatum = ablaufDatum;
-    }
-
-    // Domänenlogik
-    public boolean istAbgelaufen() {
-        return YearMonth.now().isAfter(this.ablaufDatum);
-    }
+    public boolean istAbgelaufen() { return YearMonth.now().isAfter(this.ablaufDatum); }
 
     public boolean laeuftInXWochenAb(int wochen) {
         LocalDate heute = LocalDate.now();
@@ -168,5 +92,86 @@ public class Medikament {
                     darreichungsform,
                     dosierung,
                     ablaufDatum.toString());
+    }
+
+    public static class Builder {
+        private final UniqueIdentifier ui;
+        private Seriennummer serienNummer;
+        private PZN pzn;
+        private Chargennummer chargenNummer;
+        private String medikamentenName;
+        private String wirkstoffBezeichnung;
+        private String hersteller;
+        private Darreichungsform darreichungsform;
+        private Dosierung dosierung;
+        private YearMonth ablaufDatum;
+
+        public Builder(UniqueIdentifier ui) {
+            if (ui == null) {
+                throw new IllegalArgumentException("Pharmazeutische Kennung darf nicht null sein");
+            }
+            this.ui = ui;
+        }
+
+        public Builder serienNummer(Seriennummer serienNummer) {
+            this.serienNummer = serienNummer;
+            return this;
+        }
+
+        public Builder pzn(PZN pzn) {
+            this.pzn = pzn;
+            return this;
+        }
+
+        public Builder chargenNummer(Chargennummer chargenNummer) {
+            this.chargenNummer = chargenNummer;
+            return this;
+        }
+
+        public Builder medikamentenName(String medikamentenName) {
+            this.medikamentenName = medikamentenName;
+            return this;
+        }
+
+        public Builder wirkstoffBezeichnung(String wirkstoffBezeichnung) {
+            this.wirkstoffBezeichnung = wirkstoffBezeichnung;
+            return this;
+        }
+
+        public Builder hersteller(String hersteller) {
+            this.hersteller = hersteller;
+            return this;
+        }
+
+        public Builder darreichungsform(Darreichungsform darreichungsform) {
+            this.darreichungsform = darreichungsform;
+            return this;
+        }
+
+        public Builder dosierung(Dosierung dosierung) {
+            this.dosierung = dosierung;
+            return this;
+        }
+
+        public Builder ablaufDatum(YearMonth ablaufDatum) {
+            this.ablaufDatum = ablaufDatum;
+            return this;
+        }
+
+        public Medikament build() {
+            if (serienNummer == null) {
+                throw new IllegalStateException("Seriennummer darf nicht null sein!");
+            }
+            if (pzn == null) {
+                throw new IllegalStateException("PZN darf nicht null sein!");
+            }
+            if (chargenNummer == null) {
+                throw new IllegalStateException("Chargennummer darf nicht null sein!");
+            }
+            if (ablaufDatum == null) {
+                throw new IllegalStateException("Ablaufdatum muss gesetzt sein!");
+            }
+            return new Medikament(this);
+        }
     }
 }
