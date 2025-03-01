cab14d1c2d552c48b2b64db420b681c77670a1df
Naxsos <112490751+Naxsos@users.noreply.github.com>
2025-03-01 14:40:16 +0100

    Refactoring: Data Class

--- 03-domain/src/main/java/org/example/Dosierung.java
+++ 03-domain/src/main/java/org/example/Dosierung.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Dosierung {
 
     private Masseinheit masseinheit;
@@ -16,5 +18,42 @@ public class Dosierung {
         this.wertIntervall = wertIntervall;
         this.zeitperiode = zeitperiode;
         Wertzeitperiode = wertzeitperiode;
+    }
+
+    public Masseinheit getMasseinheit() {
+        return masseinheit;
+    }
+
+    public int getWertMasseinheit() {
+        return wertMasseinheit;
+    }
+
+    public Intervall getIntervall() {
+        return intervall;
+    }
+
+    public int getWertIntervall() {
+        return wertIntervall;
+    }
+
+    public Zeitperiode getZeitperiode() {
+        return zeitperiode;
+    }
+
+    public int getWertzeitperiode() {
+        return Wertzeitperiode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dosierung dosierung = (Dosierung) o;
+        return wertMasseinheit == dosierung.wertMasseinheit && wertIntervall == dosierung.wertIntervall && Wertzeitperiode == dosierung.Wertzeitperiode && masseinheit == dosierung.masseinheit && intervall == dosierung.intervall && zeitperiode == dosierung.zeitperiode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(masseinheit, wertMasseinheit, intervall, wertIntervall, zeitperiode, Wertzeitperiode);
     }
 }
