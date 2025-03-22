22fdbcaf1fe8e071992254b6cd84b7e727649f42
Naxsos <112490751+Naxsos@users.noreply.github.com>
2025-03-22 17:11:19 +0100

    Erinnerungen and Adjustemnts for Medikament

--- 01-adapter/src/main/java/org/example/MedikamenteController.java
+++ 01-adapter/src/main/java/org/example/MedikamenteController.java
@@ -5,23 +5,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MedikamenteController {
-    private final ErstelleMedikamentUseCase erstelleMedikamentUseCase;
-    private final AlleMedikamenteAusgebenUseCase alleMedikamenteAusgebenUseCase;
-    private final EntferneMedikamentUseCase entferneMedikamentUseCase;
+    private final MedikamenteVerwaltenUseCase medikamenteVerwaltenUseCase;
+    private final FindeMedikamenteUseCase findeMedikamenteUseCase;
     private final ListeInZeitraumAblaufenderMedikamenteUseCase listeInZeitraumAblaufenderMedikamenteUseCase;
     private final AbgelaufeneMedikamenteUseCase abgelaufeneMedikamenteUseCase;
     private List<MedikamenteUI> ui;
 
     public MedikamenteController(
-            ErstelleMedikamentUseCase erstelleMedikamentUseCase,
-            AlleMedikamenteAusgebenUseCase alleMedikamenteAusgebenUseCase,
-            EntferneMedikamentUseCase entferneMedikamentUseCase,
+            MedikamenteVerwaltenUseCase medikamenteVerwaltenUseCase,
             ListeInZeitraumAblaufenderMedikamenteUseCase listeInZeitraumAblaufenderMedikamenteUseCase,
+            FindeMedikamenteUseCase findeMedikamenteUseCase,
             AbgelaufeneMedikamenteUseCase abgelaufeneMedikamenteUseCase
     ) {
-        this.erstelleMedikamentUseCase = erstelleMedikamentUseCase;
-        this.alleMedikamenteAusgebenUseCase = alleMedikamenteAusgebenUseCase;
-        this.entferneMedikamentUseCase = entferneMedikamentUseCase;
+        this.medikamenteVerwaltenUseCase = medikamenteVerwaltenUseCase;
+        this.findeMedikamenteUseCase = findeMedikamenteUseCase;
         this.listeInZeitraumAblaufenderMedikamenteUseCase = listeInZeitraumAblaufenderMedikamenteUseCase;
         this.abgelaufeneMedikamenteUseCase = abgelaufeneMedikamenteUseCase;
         this.ui = new ArrayList<>();
@@ -33,23 +30,23 @@ public class MedikamenteController {
 
     public void initialisiereMedikamentenlisten() {
         for (MedikamenteUI medikamente : ui) {
-            medikamente.aktualisiereMedikamentenliste(convertToStrings(alleMedikamenteAusgebenUseCase.listeAllerMedikamente()));
+            medikamente.aktualisiereMedikamentenliste(convertToStrings(findeMedikamenteUseCase.listeAllerMedikamente()));
             medikamente.aktualisiereBaldAblaufendeMedikamente(convertToStrings(listeInZeitraumAblaufenderMedikamenteUseCase.findeMedikamenteDieInXWochenAblaufen(2)));
             medikamente.aktualisiereAbgelaufeneMedikamente(convertToStrings(abgelaufeneMedikamenteUseCase.abgelaufeneMedikamente()));
         }
     }
 
     public void erstelleMedikament(MedikamentDTO dto) {
-        erstelleMedikamentUseCase.erstelleMedikament(
+        medikamenteVerwaltenUseCase.erstelleMedikament(
                 Integer.parseInt(dto.pzn), dto.seriennummer, dto.chargennummer, dto.name, dto.wirkstoff, dto.ablaufdatum, dto.lagerort
         );
         initialisiereMedikamentenlisten();
     }
 
     public void loescheMedikament(int index) {
-        List<Medikament> medikamente = alleMedikamenteAusgebenUseCase.listeAllerMedikamente();
+        List<Medikament> medikamente = findeMedikamenteUseCase.listeAllerMedikamente();
         if (index >= 0 && index < medikamente.size()) {
-            entferneMedikamentUseCase.medikamentEntfernen(medikamente.get(index).getUi().toString());
+            medikamenteVerwaltenUseCase.medikamentEntfernen(medikamente.get(index).getUi().toString());
         }
         initialisiereMedikamentenlisten();
     }
